3043e44d10940acd392b86dcc3782dfd234b302b
brian williams <brianw83002@csu.fullerton.edu>
2026-04-16 10:29:55 -0700

    render

--- FlaskRun/app.py
+++ FlaskRun/app.py
@@ -283,4 +283,8 @@ def result_page(task_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Render provides PORT
+    app.run(host="0.0.0.0", port=port)
+
+    
+    ###app.run(debug=True)
